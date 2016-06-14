63ae87bd653cfc5da68d9cfaf700186f3d5a270d
robotto <mark@moore.dkexit>
2016-06-14 09:15:28 +0200

    init

---
+++ meteogram.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
+#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -sketch 1 meteogram.png
+convert meteogramRaw.png -negate -shave 9x4 -chop 0x21 -sketch 2 meteogram.png
+#rm meteogramRaw.png
