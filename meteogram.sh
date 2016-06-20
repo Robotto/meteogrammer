521b6bafc379f48eb2d17e254fb00b5b078ac003
robotto <mark@moore.dkexit>
2016-06-20 22:59:27 +0200

    trying out different sharpening techniques

--- meteogram.sh
+++ meteogram.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 curl --silent http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
 #convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x37 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
-convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
+#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
+#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -unsharp 1.5×1.0+1.5+0.02 meteogram.png
+convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -adaptive-sharpen 0x3.0 meteogram.png
+
 #rm meteogramRaw.png
