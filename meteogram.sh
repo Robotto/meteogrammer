a042d6fc39ad53ec8ccc9ab980e6b128a5eaa4f4
robotto <mark@moore.dkexit>
2016-06-20 22:49:21 +0200

    fit to a 480x320 display, sharpened a bit, cropped a lot - meteogram is now 24hrs instead of 48

--- meteogram.sh
+++ meteogram.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
-#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -sketch 1 meteogram.png
-convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -sketch 2 meteogram.png
+curl --silent http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
+#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x37 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
+convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
 #rm meteogramRaw.png
