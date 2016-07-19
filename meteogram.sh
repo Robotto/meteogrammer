ec97592e2b43b9994da990038396e91b174787e6
robotto <mark@moore.dk>
2016-07-19 11:12:14 +0200

    folder specific

--- meteogram.sh
+++ meteogram.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+homedir=~
+eval homedir=$homedir
+cd $homedir/meteogrammer
 curl --silent http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
 #convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x37 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
 #convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
