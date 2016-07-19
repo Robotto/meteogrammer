cb0742a213ebcf7c5f5d3593591c4f4d9392f85d
robotto <mark@moore.dk>
2016-07-20 00:10:44 +0200

    changed gram source to advaced to get cloud cover, and fiddled with colouring

--- meteogram.sh
+++ meteogram.sh
@@ -2,11 +2,7 @@
 homedir=~
 eval homedir=$homedir
 cd $homedir/meteogrammer
-curl --silent http://www.yr.no/sted/Danmark/Midtjylland/aarhus/meteogram.png > meteogramRaw.png
-#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x37 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
-#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -sharpen 0x2.0 meteogram.png
-#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -unsharp 1.5×1.0+1.5+0.02 meteogram.png
-#convert -colorspace Gray meteogramRaw.png -negate -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -sketch 2 -resize 480x320\! -adaptive-sharpen 0x3.0 meteogram.png
-convert meteogramRaw.png -shave 9x4 -chop 0x21 -gravity East -chop 408x0 -resize 480x320\! meteogram.png
-rm meteogramRaw.png
+curl --silent http://www.yr.no/sted/Danmark/Midtjylland/aarhus/avansert_meteogram.png > meteogramRaw.png
+convert -colorspace Gray meteogramRaw.png -shave 9x4 -chop 0x35 -gravity East -chop 408x0 -resize 480x320\!  -fill yellow -tint 100 -negate meteogram.png
+#rm meteogramRaw.png
 echo "Done at $(date)"
