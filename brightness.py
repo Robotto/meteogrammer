a68718a77a1738f4ef5bf39df9c1e20788d0736b
robotto <mark@moore.dk>
2016-07-27 10:48:45 +0200

    added lowpass filter to the ldr reading, to smooth the backlight out a bit

--- brightness.py
+++ brightness.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 
-# Uses Adafruit's Example for RC timing reading for Raspberry Pi found here: 
+# Uses Adafruit's Example for RC timing reading for Raspberry Pi found here:
 # https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi/basic-photocell-reading
-# 
+#
 # The LDR timing controls the duty cycle of a PWM output to a PNP transistor controlling the backlight on a modified 3.5" LCD
 
 import RPi.GPIO as GPIO, time, os
+import numpy as np
 
 LDRpin=23
 PWMpin=18
 
 LDRtimeMAX=100000
+dutyCycleOffset=-1
+lowPassAlpha=25
+meanLdrTime=0 #startupvalue
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(PWMpin, GPIO.OUT)
 p = GPIO.PWM(PWMpin, 100) #50Hz
 p.start(100) #50 % duty cycle to start
+
 
 def RCtime (RCpin):
         reading = 0
@@ -32,18 +37,25 @@ def RCtime (RCpin):
 
 try:
     while True:
-            ldrTime=RCtime(LDRpin)
-            # print "ldrTime: " + str(ldrTime)
+            rawLdrTime=RCtime(LDRpin)
 
-            if (ldrTime>LDRtimeMAX): #safety feature
-                    ldrTime=LDRtimeMAX
+            print "rawLdrTime:  " + str(rawLdrTime)
 
-            dutyCycle=100-int(100*(LDRtimeMAX-ldrTime)/float(LDRtimeMAX)) #force float division, and then truncate back to integer
+            meanLdrTime=((meanLdrTime*lowPassAlpha)+rawLdrTime)/(lowPassAlpha+1)
 
-            # print "dutycycle: "+str(dutyCycle)
+            meanLdrTime=np.clip(meanLdrTime,0,LDRtimeMAX) #make sure it's within range
+
+            print "meanLdrTime: " + str(meanLdrTime)
+
+            dutyCycle=dutyCycleOffset+100-int(100*(LDRtimeMAX-meanLdrTime)/float(LDRtimeMAX)) #force float division, and then truncate back to integer
+
+            dutyCycle=np.clip(dutyCycle,0,100) #constrain the duty cycle to allowed values
+
+            print "dutycycle:   "+str(dutyCycle)
 
             p.ChangeDutyCycle(dutyCycle)
-            time.sleep(0.1)
+
+            #time.sleep(0.01)
 
 
 #cleanup:
