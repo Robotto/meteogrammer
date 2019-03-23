c9f36e7da17e0cdc94d2f2a0da0104af73366ab2
robotto <mark@moore.dk>
2019-03-23 16:09:05 +0100

    refresh

--- showMeteogram.sh
+++ showMeteogram.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo fbi -T 2 -d /dev/fb1 -noverbose -t 300 /home/robotto/meteogrammer/*.png -cachemem 0
-#fbi -T 2 -d /dev/fb1 -noverbose -t 300 /home/pi/meteogrammer/*.png -cachemem 0
+sudo fbi -T 2 -d /dev/fb0 -noverbose -t 300 /home/robotto/meteogrammer/*.png -cachemem 0
